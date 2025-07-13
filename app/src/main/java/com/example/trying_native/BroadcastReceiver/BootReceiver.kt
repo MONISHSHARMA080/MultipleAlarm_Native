d5e8dfcbdfba8fc72f0b8585be3781747649d8a9
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-07-13 10:35:34 +0530

    made the xml file ready now need to implement the logic

---
+++ app/src/main/java/com/example/trying_native/BroadcastReceiver/BootReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.trying_native.BroadcastReceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.trying_native.notification.NotificationBuilder
+
+class BootReceiver : BroadcastReceiver(){
+    private val TAG = "BootReceiver"
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
