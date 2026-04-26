85ec6c8b0d3e17587351147c5d7464f1bae9ff53
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-04-26 10:03:36 +0530

    fixing the github actions failed due to baseling profile 1

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 kotlin {
 	compilerOptions {
-		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+//		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
 	}
 }
 
