b0391ee6e1f90043f74d7076341710d3d230bcd5
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-04-27 17:18:03 +0530

    forgot to mark the env as debug and posthog started to send the event in the baselineProfile

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 			localDevices {
 				create("pixel6Api31") {
 					device = "Pixel 8a"
-					apiLevel = 33
+					apiLevel = 34
 					systemImageSource = "google"
 				}
 			}
@@ -68,67 +68,3 @@ androidComponents {
 		)
 	}
 }
-
-
-// --- adding the gradle managed device and asking claude to do vibe coding
-
-//plugins {
-//	alias(libs.plugins.android.test)
-//	alias(libs.plugins.baselineprofile)
-//	alias(libs.plugins.kotlin.android)
-//}
-//
-//android {
-//	namespace = "com.example.baselineprofile"
-//	compileSdk {
-//		version = release(36) {
-//			minorApiLevel = 1
-//		}
-//	}
-//
-//	compileOptions {
-//		sourceCompatibility = JavaVersion.VERSION_25
-//		targetCompatibility = JavaVersion.VERSION_25
-//	}
-//
-//	defaultConfig {
-//		minSdk = 28
-//		targetSdk = 36
-//
-//		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//	}
-//
-//	targetProjectPath = ":app"
-//
-//}
-//
-//kotlin {
-//	compilerOptions {
-//		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25)
-//	}
-//}
-//
-//// This is the configuration block for the Baseline Profile plugin.
-//// You can specify to run the generators on a managed devices or connected devices.
-//baselineProfile {
-//}
-//
-//dependencies {
-//	implementation(libs.androidx.junit)
-//	implementation(libs.androidx.espresso.core)
-//	implementation(libs.androidx.uiautomator)
-//	implementation(libs.androidx.benchmark.macro.junit4)
-//	implementation(libs.core.ktx)
-//
-//
-//}
-//
-//androidComponents {
-//	onVariants { v ->
-//		val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
-//		v.instrumentationRunnerArguments.put(
-//			"targetAppId",
-//			v.testedApks.map { artifactsLoader.load(it)?.applicationId }
-//		)
-//	}
-//}
