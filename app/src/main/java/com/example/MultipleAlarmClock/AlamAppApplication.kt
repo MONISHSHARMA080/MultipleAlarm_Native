492f0cdf6d47bb1c9faad351e3decd331bd92389
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-05-27 17:44:28 +0530

    good god please work, fixed the hilt one

--- app/src/main/java/com/example/MultipleAlarmClock/AlamAppApplication.kt
+++ app/src/main/java/com/example/MultipleAlarmClock/AlamAppApplication.kt
@@ -6,19 +6,14 @@ import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import jakarta.inject.Inject
 
-
 @HiltAndroidApp
 class AlarmApp : Application(), Configuration.Provider {
 
 	@Inject
-	lateinit var workerFactory: HiltWorkerFactory  // ← inject Hilt's factory
-
-	override fun onCreate() {
-		super.onCreate()
-	}
+	lateinit var workerFactory: HiltWorkerFactory
 
 	override val workManagerConfiguration: Configuration
 		get() = Configuration.Builder()
-			.setWorkerFactory(workerFactory)        // ← needed for Hilt workers
+			.setWorkerFactory(workerFactory)
 			.build()
 }
