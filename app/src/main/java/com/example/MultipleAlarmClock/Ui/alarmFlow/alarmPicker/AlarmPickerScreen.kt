21582cf9fe5ce0916dff9a61835e0f24ecabaed6
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-06-11 11:14:05 +0530

    added the frequency one now need to work on message and animations

--- app/src/main/java/com/example/MultipleAlarmClock/Ui/alarmFlow/alarmPicker/AlarmPickerScreen.kt
+++ app/src/main/java/com/example/MultipleAlarmClock/Ui/alarmFlow/alarmPicker/AlarmPickerScreen.kt
@@ -15,37 +15,46 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.automirrored.rounded.Label
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Remove
+import androidx.compose.material.icons.rounded.Timer
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalLocale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.coolApps.MultipleAlarmClock.dataBase.AlarmData
+import com.coolApps.MultipleAlarmClock.dataBase.AlarmErrorField
 import com.coolApps.MultipleAlarmClock.dataBase.ValidationResult
+import com.coolApps.MultipleAlarmClock.logD
 import com.example.MultipleAlarmClock.Ui.alarmPicker.AlarmPickerViewModel
 import java.text.SimpleDateFormat
-import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -58,17 +67,12 @@ fun AlarmPickerScreen(
 	val uiState by viewModel.uiState.collectAsState()
 	val selectedSound by viewModel.selectedAlarmSound.collectAsState()
 
-	LaunchedEffect(alarm) {
-		viewModel.setInitialAlarmObject(alarm)
-	}
-
-	val timeFormat = remember { SimpleDateFormat("h:mm", Locale.getDefault()) }
-	val amPmFormat = remember { SimpleDateFormat("a", Locale.getDefault()) }
 	val startTime = uiState.alarmObject.startTime.time
 	val endTime = uiState.alarmObject.endTime.time
 
 	val timeStyle = MaterialTheme.typography.displayLarge.copy(fontWeight = FontWeight.Bold)
-	val amPmStyle = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Medium)
+	val amPmStyle = MaterialTheme.typography.labelLarge
+	val currentError by remember(uiState) { mutableStateOf(  uiState.validationResult as? ValidationResult.Failure) }
 
 	Scaffold { screenPadding->
 		Column(
@@ -85,7 +89,6 @@ fun AlarmPickerScreen(
 			) {
 				Row(
 					verticalAlignment = Alignment.Bottom,
-//					horizontalArrangement = Arrangement.spacedBy(4.dp),
 					modifier = Modifier.weight(1f)
 				) {
 					Text(
@@ -138,7 +141,7 @@ fun AlarmPickerScreen(
 
 			DateList(
 				{}, startTime.time,
-				weGood = uiState.validationResult == ValidationResult.Success,
+				weGood = currentError?.field != AlarmErrorField.DATE,
 				allowSelectingPastDate = false,
 			)
 
@@ -168,6 +171,27 @@ fun AlarmPickerScreen(
 						title = "Sound",
 						value = selectedSound?.title ?: "Random",
 						onClick = onNavigateToSoundList
+					)
+
+					HorizontalDivider(
+						modifier = Modifier.padding(horizontal = 16.dp),
+						color = MaterialTheme.colorScheme.outlineVariant,
+					)
+
+					FrequencyRow(
+						icon = Icons.Rounded.Timer,
+						title = "Repeat every",
+						value = uiState.alarmObject.freqGottenAfterCallback,
+						onValueChange = { newValue ->
+							newValue.let {
+								logD("new freq: value is $it")
+								if (it in 0..<720){
+									viewModel.updateFrequency(it)
+								}
+							}
+						},
+						previewText = viewModel.getFrequencyPreviewText(),
+						validationResult = currentError,
 					)
 				}
 			}
@@ -212,7 +236,6 @@ fun AlarmPickerScreen(
 				}
 			}
 		}
-
 	}
 
 }
@@ -255,32 +278,91 @@ private fun SettingRow(
 private fun FrequencyRow(
 	icon: ImageVector,
 	title: String,
-	value: String,
-	onClick: () -> Unit
+	value: Long,
+	onValueChange: (Long) -> Unit,
+	previewText: String = "",
+	validationResult: ValidationResult.Failure?
 ) {
-	Row(
+	val colorScheme = MaterialTheme.colorScheme
+	val doWeHaveError =validationResult?.field == AlarmErrorField.FREQUENCY
+
+	Column(
 		modifier = Modifier
 			.fillMaxWidth()
-			.clickable(onClick = onClick)
-			.padding(horizontal = 16.dp, vertical = 20.dp),
-		verticalAlignment = Alignment.CenterVertically
+			.padding(horizontal = 16.dp, vertical = 12.dp)
 	) {
-		Icon(
-			imageVector = icon,
-			contentDescription = null,
-			tint = MaterialTheme.colorScheme.onSurfaceVariant
-		)
-		Spacer(modifier = Modifier.width(16.dp))
-		Text(
-			text = title,
-			color = MaterialTheme.colorScheme.onBackground,
-			fontSize = 16.sp
-		)
-		Spacer(modifier = Modifier.weight(1f))
-		Text(
-			text = value,
-			color = MaterialTheme.colorScheme.onSurfaceVariant,
-			fontSize = 14.sp
-		)
+		Row(
+			modifier = Modifier.fillMaxWidth(),
+			verticalAlignment = Alignment.CenterVertically
+		) {
+			Icon(
+				imageVector = icon,
+				contentDescription = null,
+				tint = MaterialTheme.colorScheme.onSurfaceVariant
+			)
+			Spacer(modifier = Modifier.width(16.dp))
+			Text(
+				text = title,
+				color = MaterialTheme.colorScheme.onBackground,
+				fontSize = 16.sp,
+				modifier = Modifier.weight(1f)
+			)
+
+			// Expressive Frequency Picker
+			Row(
+				verticalAlignment = Alignment.CenterVertically,
+				modifier = Modifier
+					.background(
+						color = if (doWeHaveError) colorScheme.errorContainer else colorScheme.primaryContainer ,
+						shape = RoundedCornerShape(12.dp)
+					)
+					.padding(4.dp)
+			) {
+				IconButton(
+					onClick = {onValueChange(value -1)},
+					modifier = Modifier.size(36.dp)
+				) {
+					Icon(
+						imageVector = Icons.Rounded.Remove,
+						contentDescription = "Decrease",
+						tint = MaterialTheme.colorScheme.onPrimaryContainer
+					)
+				}
+
+				BasicTextField(
+					value = if (value == 0L) "" else value.toString(),
+					onValueChange = {newValue -> newValue.toLongOrNull()?.let { onValueChange(it)} ?: onValueChange(0);  },
+					modifier = Modifier.width(45.dp),
+					textStyle = MaterialTheme.typography.titleMedium.copy(
+						textAlign = TextAlign.Center,
+						color = MaterialTheme.colorScheme.onPrimaryContainer,
+						fontWeight = FontWeight.Bold
+					),
+					keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+					singleLine = true
+				)
+
+				IconButton(
+					onClick = {onValueChange(value + 1)},
+					modifier = Modifier.size(36.dp)
+				) {
+					Icon(
+						imageVector = Icons.Rounded.Add,
+						contentDescription = "Increase",
+						tint = MaterialTheme.colorScheme.onPrimaryContainer
+					)
+				}
+			}
+		}
+
+		if (previewText.isNotEmpty()) {
+			// add a slight animation here
+			Text(
+				text = previewText,
+				style = MaterialTheme.typography.labelSmall,
+				color = if (doWeHaveError) colorScheme.onErrorContainer else colorScheme.onSurfaceVariant,
+				modifier = Modifier.padding(start = 40.dp, top = 2.dp)
+			)
+		}
 	}
 }
