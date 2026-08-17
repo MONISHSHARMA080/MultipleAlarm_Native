c5cffcc9ce5ce6aff9900f7a018750568c8397ca
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-08-17 16:52:25 +0530

    changed the message to be a bit smaller

--- app/src/main/java/MultipleAlarmClock/alarmFeature/activities/AlarmActivity.kt
+++ app/src/main/java/MultipleAlarmClock/alarmFeature/activities/AlarmActivity.kt
@@ -288,7 +288,7 @@ class AlarmActivity : ComponentActivity() {
 								Text(
 									text = message,
 									color = colorScheme.onSurface,
-									style = typography.headlineLarge,
+									style = typography.headlineMedium,
 									fontWeight = FontWeight.Normal,
 									lineHeight = 35.sp
 								)
