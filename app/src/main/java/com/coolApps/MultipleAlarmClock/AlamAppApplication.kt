6f43f671b06790c7adf2ca7d7eabbc2eff2530f7
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-08-21 12:26:04 +0530

    removed the on crete override inApplication(), working fine on plixel 10 pro xl emulator too ,

--- app/src/main/java/com/coolApps/MultipleAlarmClock/AlamAppApplication.kt
+++ app/src/main/java/com/coolApps/MultipleAlarmClock/AlamAppApplication.kt
@@ -3,8 +3,6 @@ package com.coolApps.MultipleAlarmClock
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
-import com.google.firebase.FirebaseApp
-import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 import jakarta.inject.Inject
 
@@ -19,20 +17,21 @@ class AlarmApp : Application(), Configuration.Provider {
 			.setWorkerFactory(workerFactory)
 			.build()
 
-	override fun onCreate() {
-		super.onCreate()
-		val options = FirebaseApp.getInstance().options
-		logD("Options:$options, ")
-		FirebaseMessaging.getInstance().register().addOnCompleteListener { task ->
-			if (!task.isSuccessful) {
-				logD( "Fetching FCM registration token failed ${task.exception?.message}\n result:${task.result}, stackTrace:${task.exception?.stackTrace},  " )
-				return@addOnCompleteListener
-			}
-
-			// Get new FCM registration token
-			val token = task.result
-			logD( "FCM Token: $token")
-			// Send this token to your server if required
-		}
-	}
+//	override fun onCreate() {
+//		super.onCreate()
+////		val options = FirebaseApp.getInstance().options
+////		logD("Options:$options, ")
+////		FirebaseMessaging.getInstance().register().addOnCompleteListener { task ->
+////			if (!task.isSuccessful) {
+////				logD("Fetching FCM registration failed: ${task.exception?.message}")
+////				task.exception?.printStackTrace()
+////				return@addOnCompleteListener
+////			}
+////
+////			// Registration successful.
+////			// Note: result is Void for register() task.
+////			// The actual token (FID) is delivered via FirebaseMessagingService.onRegistered(String)
+////			logD("FCM Registration successful")
+////		}
+//	}
 }
