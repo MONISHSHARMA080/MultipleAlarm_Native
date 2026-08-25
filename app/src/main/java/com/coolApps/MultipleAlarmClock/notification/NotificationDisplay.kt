5bdd1ed32a409d1dd10875799a7681eef80ca162
MONISHSHARMA080 <monishsharma080@gmail.com>
2026-08-25 19:17:40 +0530

    will open the main activity if opened the app

--- app/src/main/java/com/coolApps/MultipleAlarmClock/notification/NotificationDisplay.kt
+++ app/src/main/java/com/coolApps/MultipleAlarmClock/notification/NotificationDisplay.kt
@@ -3,8 +3,11 @@ package com.coolApps.MultipleAlarmClock.notification
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.coolApps.MultipleAlarmClock.MainActivity
 import com.coolApps.MultipleAlarmClock.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import jakarta.inject.Inject
@@ -55,14 +58,35 @@ class NotificationHandler @Inject constructor(@ApplicationContext val context: C
 	 private var notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
 	fun build(
-		notificationChannel: NotificationChannelType, notificationTitle: String, notificationText: String
+			contentIntent: PendingIntent? = createDefaultPendingIntent(),
+			notificationChannel: NotificationChannelType, notificationTitle: String, notificationText: String,
 	): Notification {
-		return NotificationCompat.Builder(context, notificationChannel.channelId )
+
+		return NotificationCompat.Builder(context, notificationChannel.channelId)
 			.setSmallIcon(R.mipmap.app_icon)
 			.setContentTitle(notificationTitle)
 			.setContentText(notificationText)
 			.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+			.setAutoCancel(true) // Automatically dismisses notification when tapped
+			.apply {
+				if (contentIntent != null) {
+					setContentIntent(contentIntent) // Launches app when tapped
+				}
+			}
 			.build()
+	}
+
+	private fun createDefaultPendingIntent(): PendingIntent? {
+		val intent = Intent(context, MainActivity::class.java).apply {
+			flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+		}
+		return PendingIntent.getActivity(
+			context,
+			0,
+			intent,
+			PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+		)
+
 	}
 
 	private fun createNotificationChannel(notificationChannelDetail: NotificationChannelType){
